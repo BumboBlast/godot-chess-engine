2814f339ca45b4449ccaaea3ea4835526015c1f7
BumboBlast <john.jameschar@gmail.com>
2022-10-12 21:25:13 -0400

    Refactored. BoardSquares
    Began mapping board squares to coords relative to the board position on the screen

---
+++ ChessBoard.gd
@@ -0,0 +1,73 @@
+extends Node2D
+
+
+"""
+1. create piece nodes
+"""
+func instanceNode2D(name):
+	#make the nodes themselves
+	var piece = Node2D.new()
+	piece.set_name(name)
+	
+	#give them sprite-nodes
+	var sprite = Sprite.new()
+	piece.add_child(sprite)
+	sprite.set_name(name + " sprite")
+	add_child(piece)
+	
+func createPiece(name, amount):
+	for n in amount:
+		var pawnName = name + String(n)
+		instanceNode2D(pawnName)
+
+"""
+2a. make a list of board locations
+2b. set piece textures and starting positions on the board
+"""
+
+var boardRect
+
+var squares = {}
+
+func createSquares():
+	
+	# get board rect coords
+
+	
+	
+	for rank in 8:
+		for file in 8:
+			var newSquare = String(char(rank + 65)) + String(char(file + 49))
+			squares[newSquare] = 10
+			print(newSquare, ",", squares[newSquare])
+
+
+func startBoard():
+	createPiece("Pawn", 16)
+	createPiece("Rook", 4)
+	createPiece("Knight", 4)
+	createPiece("Bishop", 4)
+	createPiece("King", 2)
+	createPiece("Queen", 2)
+	createSquares()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	startBoard()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print(boardRect)
+#	pass
+
+func updateBoardRect():
+	var rpos = get_node("BoardTexture").rect_position
+	var rsiz = get_node("BoardTexture").rect_size
+	boardRect = Rect2(rpos,rsiz)
+
+func _on_BoardTexture_ready():
+	updateBoardRect()
+	pass # Replace with function body.
+
