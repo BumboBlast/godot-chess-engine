a459de280938baeb9cf09834ef894728ebe7777c
BumboBlast <john.jameschar@gmail.com>
2022-10-12 22:33:27 -0400

    Got a piece to appear on the board

--- ChessBoard.gd
+++ ChessBoard.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
 
+
+var boardRect
+
+var pieces = []
+
 """
 1. create piece nodes
 """
@@ -15,32 +20,60 @@ func instanceNode2D(name):
 	sprite.set_name(name + " sprite")
 	add_child(piece)
 	
+	pieces.push_back(piece)
+
+
 func createPiece(name, amount):
 	for n in amount:
 		var pawnName = name + String(n)
 		instanceNode2D(pawnName)
+
+func givePiecesTextures():
+	var tex = load("res://art/debugPiecesSpriteSheet1.png")
+	var pieceFromSpriteSheet = Rect2(0,0,8,8)
+	for p in pieces:
+		p.get_child(0).set_region(1)
+		p.get_child(0).set_region_rect(pieceFromSpriteSheet)
+		p.get_child(0).texture = tex
 
 """
 2a. make a list of board locations
 2b. set piece textures and starting positions on the board
 """
 
-var boardRect
+func updateBoardRect():
+	var rpos = get_node("BoardTexture").rect_position
+	var rsiz = get_node("BoardTexture").rect_size
+	boardRect = Rect2(rpos,rsiz)
+
+
+func _on_BoardTexture_ready():
+	updateBoardRect()
+	pass # Replace with function body.
+
+
+
+func calculateSquareCoords(rank, file):
+	var newXpos = (rank * 7)
+	var newYpos = (file * 7)
+	var newWidth = (boardRect.size[1] / 7)
+	var newHeight = (boardRect.size[0] / 7)
+	return Rect2(newXpos, newYpos, newWidth, newHeight)
+			
 
 var squares = {}
-
 func createSquares():
-	
-	# get board rect coords
-
-	
 	
 	for rank in 8:
 		for file in 8:
 			var newSquare = String(char(rank + 65)) + String(char(file + 49))
-			squares[newSquare] = 10
-			print(newSquare, ",", squares[newSquare])
+			squares[newSquare] = calculateSquareCoords(rank, file)
+			#print(newSquare, ",", squares[newSquare])
 
+func startPiecesOnSquares():
+	for p in pieces:
+		p.position = Vector2(100,100)
+		pass # <-------------------------------------------------------------
 
 func startBoard():
 	createPiece("Pawn", 16)
@@ -49,7 +82,10 @@ func startBoard():
 	createPiece("Bishop", 4)
 	createPiece("King", 2)
 	createPiece("Queen", 2)
+	givePiecesTextures()
 	createSquares()
+	startPiecesOnSquares()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -59,15 +95,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(boardRect)
-#	pass
-
-func updateBoardRect():
-	var rpos = get_node("BoardTexture").rect_position
-	var rsiz = get_node("BoardTexture").rect_size
-	boardRect = Rect2(rpos,rsiz)
-
-func _on_BoardTexture_ready():
-	updateBoardRect()
-	pass # Replace with function body.
+	pass
 
